2279104e0aaa18c876835cf6e45d0e6977cae977
NatG <101484974+Natalia24v@users.noreply.github.com>
2023-11-07 19:24:10 -0300

    error 2

--- Frontend/app/src/main/java/com/curso/android/app/practica/aplicacionispc/Registro.java
+++ Frontend/app/src/main/java/com/curso/android/app/practica/aplicacionispc/Registro.java
@@ -1,12 +1,16 @@
 package com.curso.android.app.practica.aplicacionispc;
 
-import android.os.Bundle;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.w3c.dom.Text;
 
 public class Registro extends AppCompatActivity {
 
@@ -20,6 +24,24 @@ public class Registro extends AppCompatActivity {
         btnEnviarRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Inicializa el DBHelper
+                DatabaseHelper dbHelper = new DatabaseHelper(Registro.this);
+
+                // Obtiene una instancia de escritura de la base de datos
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                // Crea un ContentValues con los valores que deseas insertar
+                ContentValues values = new ContentValues();
+                values.put(DatabaseHelper.COLUMN_NAME, "Nombre de Prueba");
+                values.put(DatabaseHelper.COLUMN_APELLIDO, "Apellido de Prueba");
+                values.put(DatabaseHelper.COLUMN_EMAIL, "prueba@email.com");
+
+                // Inserta los valores en la tabla
+                long newRowId = db.insert(DatabaseHelper.TABLE_USUARIO, null, values);
+
+                // Cierra la base de datos cuando hayas terminado
+                db.close();
+
                 // Iniciar la actividad principal
                 Intent intent = new Intent(Registro.this, MainActivity.class);
                 startActivity(intent);
@@ -35,4 +57,6 @@ public class Registro extends AppCompatActivity {
             }
         });
     }
-}
+
+    }
+
